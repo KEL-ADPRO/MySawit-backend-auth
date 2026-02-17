46ec8b111c0c9bfd5a86863e72e3907f35ad6b5b
NaviCo-de <aridarrell30@gmail.com>
2026-02-17 23:15:47 +0700

    Early setup

---
+++ src/main/java/com/mysawit/mysawit_auth/MySawitAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mysawit.mysawit_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySawitAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySawitAuthApplication.class, args);
+	}
+
+}

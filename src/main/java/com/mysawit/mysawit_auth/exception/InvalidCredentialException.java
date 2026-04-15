8d965eea4f8950535731e4b7360db6066cff219d
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-04-15 19:35:09 +0700

    Added a servial version UID to InvalidCredentialException class

--- src/main/java/com/mysawit/mysawit_auth/exception/InvalidCredentialException.java
+++ src/main/java/com/mysawit/mysawit_auth/exception/InvalidCredentialException.java
@@ -1,6 +1,11 @@
 package com.mysawit.mysawit_auth.exception;
 
+import java.io.Serial;
+
 public class InvalidCredentialException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public InvalidCredentialException() {
         super("Invalid credentials");
     }

5195a2cdf4ed989cb4dccce5e60254f8baa39d93
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-04-16 02:07:42 +0700

    Relaxed the User model's password column

--- src/main/java/com/mysawit/mysawit_auth/model/User.java
+++ src/main/java/com/mysawit/mysawit_auth/model/User.java
@@ -29,7 +29,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String password;
 
     @Column(unique = true)

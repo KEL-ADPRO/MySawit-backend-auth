3ccdb7455a972fb39298f9dcd3865b5f318ec345
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-05-06 21:26:21 +0700

    Created tests for token blacklisting

---
+++ src/test/java/com/mysawit/mysawit_auth/util/TokenBlacklistTest.java
@@ -0,0 +1,31 @@
+package com.mysawit.mysawit_auth.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TokenBlacklistTest {
+    private TokenBlacklist tokenBlacklist;
+
+    @BeforeEach
+    void setUp() {
+        tokenBlacklist = new TokenBlacklist();
+    }
+
+    @Test
+    void freshTokenIsNotBlacklisted() {
+        assertFalse(tokenBlacklist.isBlacklisted("fresh.jwt.token"));
+    }
+
+    @Test
+    void blacklistedTokenIsRecognised() {
+        tokenBlacklist.blacklist("blacklisted.jwt.token");
+        assertTrue(tokenBlacklist.isBlacklisted("blacklisted.jwt.token"));
+    }
+
+    @Test
+    void blacklistingOneTokenDoesNotAffectAnother() {
+        tokenBlacklist.blacklist("token.a");
+        assertFalse(tokenBlacklist.isBlacklisted("token.b"));
+    }
+}

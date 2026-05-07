f0f4d0b5c02cf39e157c323d145c3154f348bfe7
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-05-07 13:50:07 +0700

    Created repository for blacklisted tokens

---
+++ src/main/java/com/mysawit/mysawit_auth/repository/BlacklistedTokenRepository.java
@@ -0,0 +1,10 @@
+package com.mysawit.mysawit_auth.repository;
+
+import com.mysawit.mysawit_auth.model.BlacklistedToken;
+import java.time.Instant;
+
+public interface BlacklistedTokenRepository {
+    void save(BlacklistedToken token);
+    boolean existsByTokenAndExpiresAtAfter(String token, Instant now);
+    void deleteExpired(Instant now);
+}

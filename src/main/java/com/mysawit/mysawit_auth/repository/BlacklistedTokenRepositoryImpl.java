7e53c18d571082d22fde0cc268960dccfa79029e
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-05-07 13:53:29 +0700

    Implemented BlacklistedTokenRepository

--- src/main/java/com/mysawit/mysawit_auth/repository/BlacklistedTokenRepositoryImpl.java
+++ src/main/java/com/mysawit/mysawit_auth/repository/BlacklistedTokenRepositoryImpl.java
@@ -1,26 +1,42 @@
 package com.mysawit.mysawit_auth.repository;
 
 import com.mysawit.mysawit_auth.model.BlacklistedToken;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
+import java.util.List;
 
 @Repository
 @NoArgsConstructor
 public class BlacklistedTokenRepositoryImpl implements BlacklistedTokenRepository {
-    @Override
-    public void save(BlacklistedToken token) {
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    @Override
+    public void save(final BlacklistedToken token) {
+        entityManager.merge(token);
     }
 
     @Override
-    public boolean existsByTokenAndExpiresAtAfter(String token, Instant now) {
-        return false;
+    public boolean existsByTokenAndExpiresAtAfter(final String token, final Instant now) {
+        List<BlacklistedToken> results = entityManager.createQuery(
+                        "SELECT b FROM BlacklistedToken b " +
+                                "WHERE b.token = :token AND b.expiresAt > :now",
+                        BlacklistedToken.class)
+                .setParameter("token", token)
+                .setParameter("now", now)
+                .getResultList();
+        return !results.isEmpty();
     }
 
     @Override
-    public void deleteExpired(Instant now) {
-
+    public void deleteExpired(final Instant now) {
+        entityManager.createQuery(
+                        "DELETE FROM BlacklistedToken b WHERE b.expiresAt < :now")
+                .setParameter("now", now)
+                .executeUpdate();
     }
 }

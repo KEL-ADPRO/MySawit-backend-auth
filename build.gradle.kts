3d2ecc4f6ef61b2368066fb5edb22a02fc358a8a
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2026-05-22 19:11:39 +0700

    Setup Prometheus and Grafana for monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
 	implementation("me.paulschwarz:spring-dotenv:4.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 	compileOnly("org.projectlombok:lombok")
